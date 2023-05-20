bc514eab0e8dec5ed1be8388f6a8111f4eba57f8
Joseph Alejandro Vargas Tordoya <Josephvargastordoya@gmail.com>
2023-05-20 17:04:07 -0400

    registro de ofertas funcional

---
+++ Frontend/src/components/pagescli/Ofertas.js
@@ -0,0 +1,81 @@
+
+import React , {Component, useState}from "react";
+import axios from "axios";
+import '../../css/estilos.css'
+import { Boton } from "../../elementos/Formularios";
+import VistaDetallada from "../VistaDetallada";
+
+class Ofertas extends  Component{
+    
+    constructor(props){
+        super(props);
+        this.state={
+            productos:[],
+            showModal:false,
+            productoSelec:"",
+            cantidad:0,
+            codigoP:-1,
+
+        }
+        this.getProductos = this.getProductos.bind(this);
+        
+    }
+    
+
+    
+    componentDidMount(){
+        this.getProductos();
+       
+    }
+    getProductos=async()=>{
+        await axios.get('http://127.0.0.1:8000/api/getOferta')
+        .then(res=>{
+            this.setState({productos: res.data.oferta});
+            console.log(this.state.productos.codcat)
+        }).catch((error)=>{
+            console.log(error);
+        });
+    }
+    
+    
+    handleReset = () => {
+        window.location.href = '/home';
+    }
+    
+    openModal = (producto) => {
+        this.setState({ showModal: true, productoSelec: producto });
+      }
+    
+    closeModal = () => {
+        this.setState({ showModal: false });
+      }
+    
+    render(){
+        
+
+        return(
+            <body id = "bodyCard">
+                
+                <br></br>
+               
+                  
+                  
+                    {
+
+                      
+                        this.state.productos.map((product)=>{
+                           
+                                <h2>{product.nombre}</h2>
+                               
+                               }
+                        ) 
+                            }
+                     
+            
+            
+               </body>
+            
+            )
+        }
+    }
+export default Ofertas;
