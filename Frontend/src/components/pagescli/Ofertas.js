491e67a1436b302afb3fa0b2cda2d69055d28040
Fabsland1a <fabybelencespedest@gmail.com>
2023-05-23 05:27:22 -0400

    Cambios3

--- Frontend/src/components/pagescli/Ofertas.js
+++ Frontend/src/components/pagescli/Ofertas.js
@@ -33,8 +33,6 @@ class Ofertas extends  Component{
         });
     }
     
-<<<<<<< HEAD
-=======
 
     getImg(codprod) {
         return axios.get(`http://127.0.0.1:8000/api/obtenerProductos/${codprod}`)
@@ -65,7 +63,6 @@ class Ofertas extends  Component{
 
 
     
->>>>>>> e9a2148af6ae29eaa610d144b87dec4fc10f3148
     handleReset = () => {
         window.location.href = '/home';
     }
@@ -87,15 +84,11 @@ class Ofertas extends  Component{
 
     };
 
-<<<<<<< HEAD
-    updateProducto = async (codoferta,fecha,fecha1,fecha2,codprod,desc,image,nombre,precioventa) => {
-=======
     
 
 
 
     updateProducto = async (codoferta,fecha,fecha1,fecha2,codprod,desc,image,nombre,precioventa,precioanterior) => {
->>>>>>> e9a2148af6ae29eaa610d144b87dec4fc10f3148
         if(fecha == fecha1 ){
             await axios
             .put('http://127.0.0.1:8000/api/putOferta/'+codoferta, {
@@ -117,24 +110,6 @@ class Ofertas extends  Component{
         }else{
             if(fecha == fecha2){
                 await axios
-<<<<<<< HEAD
-                .put('http://127.0.0.1:8000/api/putOferta/'+codoferta, {
-                    'codprod':codprod,
-                    'desc':desc,
-                    'fechaini':fecha1,
-                    'fechafin':fecha2,
-                    'precioventa': precioventa,
-                    'estado':0,
-                    'nombre':nombre,
-                    'image': image,
-                })
-                .then((res) => {
-                    console.log(res.data);
-                })
-                .catch((error) => {
-                    console.log(error);
-                });
-=======
           .put('http://127.0.0.1:8000/api/putOferta/'+codoferta, {
             'codprod':codprod,
             'desc':desc,
@@ -152,7 +127,6 @@ class Ofertas extends  Component{
           .catch((error) => {
             console.log(error);
           });
->>>>>>> e9a2148af6ae29eaa610d144b87dec4fc10f3148
             }else{
                 console.log("no hay nada que hacer");
             }
@@ -178,33 +152,6 @@ class Ofertas extends  Component{
 
                     .map((product)=>{
                         let fechaActual = new Date().toISOString().slice(0, 10);
-<<<<<<< HEAD
-                        this.updateProducto(product.codoferta,fechaActual,product.fechaini,product.fechafin,product.codprod,product.desc,product.image,product.nombre,product.precioventa);
-                        console.log(fechaActual);
-                        if(product.estado == 1){ //pregunta si la oferta esta activa
-                            return(
-                                <div class="producto" id="tarjetasOf" 
-                                    onMouseEnter={this.handleCardMouseEnter}
-                                    onMouseLeave={this.handleCardMouseLeave}
-                                    onClick={() => this.openModal(product,product.codprod)}>
-                                    <center>
-                                        <div >
-                                            <center>
-                                                <img  src={product.image}/>
-                                                <br/>
-                                                <span id="porci">aqui va el porciento %</span>
-                                                <h2 id="labelTi">{product.nombre}</h2>
-                                                <p id="labelOf">Finaliza el: {product.fechafin} </p>
-                                                <span id="precioA">Antes: </span>
-                                                <span id="precioH">Ahora: </span> <br/>
-                                                <Boton type="button" id="borrarP" className="btn"
-                                                    style={{ display: this.state.hoveredCard ? "block" : "none" }}
-                                                > Ver </Boton>
-                                            </center>
-                                        </div>
-                                    </center>
-                                </div>
-=======
                         this.updateProducto(product.codoferta,fechaActual,product.fechaini,product.fechafin,product.codprod,product.desc,product.image,product.nombre,product.precioventa,product.precioanterior);
                         let precio = this.obtenerImagen(product.codprod);
                         console.log(precio);
@@ -235,7 +182,6 @@ class Ofertas extends  Component{
                     </div>
                     </center>
                     </div>
->>>>>>> e9a2148af6ae29eaa610d144b87dec4fc10f3148
                             )
                         }
                     })
