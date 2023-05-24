e1ed87aea863febd714b88c321a9229fd56eaa30
andres7o7 <andresrevollohuanca@gmail.com>
2023-05-23 21:31:01 -0400

    carrito

--- Frontend/src/components/Carrito.js
+++ Frontend/src/components/Carrito.js
@@ -18,12 +18,13 @@ class Carrito extends Component {
       total:0,
       id:0,
       vacio:true,
+      despacho:0,
       
     }
     this.getProductos = this.getProductos.bind(this);
     this.aumentarsubtotal= this.aumentarsubtotal.bind(this);
     this.eliminar= this.eliminar.bind(this);
-   
+    this.aumentarDespacho= this.aumentarDespacho.bind(this);
   
   }
   componentDidMount() {
@@ -43,7 +44,7 @@ class Carrito extends Component {
         console.log(error);
       });
       this.aumentarsubtotal();
-     
+      this.aumentarDespacho();
   }
  
  eliminar=async(cod)=>{
@@ -52,7 +53,17 @@ class Carrito extends Component {
   this.getProductos();
     
  }
+ aumentarDespacho= () =>{
+  this.setState({despacho:0});
+  const { productos } = this.state;
+  let despacho = 0;
+  
+  productos.forEach((producto) => {
+    despacho += parseInt(producto.cantidadprod) ;
+  });
 
+  this.setState({ despacho });
+}
   aumentarsubtotal= () =>{
     this.setState({subtotal:0});
     const { productos } = this.state;
@@ -102,62 +113,52 @@ class Carrito extends Component {
             <h4 className="modal-title" id="tituloCa">Carrito</h4>
           </Modal.Header>
           <ModalBody className="modal-body">
+          {vacio ? (
+            <div className="row" id="vacio">
+              <div className="icono">
+              <BsFillCartXFill/>
+              </div>
+              <div className="textoIcono">
+              <h2>Tu carrito se encuentra vacio</h2>
+              </div>
             
-          <div className="lista">
-             
-            {
-
-
-              this.state.productos?.sort((o1, o2) => {
-                if (o1.producto < o2.producto) {
-                  return -1;
-                } else {
-                  if (o1.producto > o2.producto) {
-                    return 1;
-                  } else {
-                    return 0;
-                  }
-                }
-              })
-                .map((product, index) => {
-                  
-                 
-                  return (
-                    
-                    <div className="contenido " id="card">
-                    
-                           
-                          <div className="imagen">
-                           
-                          <img src={ this.getImg} />
-                          </div>
-                          
-                          <div className="precio">
-                          
-                          <h2>{this.name}</h2>
-                          <p>Bs. {product.costodetalle} </p>
-                          <p>{this.state.id}</p>
-                          </div>
-                          <div className="basura">
-                          <a onClick={() => this.eliminar(product.codetalle)}><BsFillTrash3Fill/></a>
-                          </div>
-
-                     
-                    </div>
-
-                  )
-
-
-
-
-                }
-                )
-            }
-   
-               
             </div>
-            
-         
+           
+          ) : (
+            <div className="lista">
+              {this.state.productos
+                ?.sort((o1, o2) => {
+                  if (o1.producto < o2.producto) {
+                    return -1;
+                  } else {
+                    if (o1.producto > o2.producto) {
+                      return 1;
+                    } else {
+                      return 0;
+                    }
+                  }
+                })
+                .map((product, index) => {
+                  return (
+                    <div className="contenido" id="card">
+                      <div className="imagen">
+                        <img src={this.getImg} />
+                      </div>
+                      <div className="precio">
+                        <h2>{this.name}</h2>
+                        <p>Bs. {product.costodetalle}</p>
+                        <p>{product.cantidadprod}</p>
+                      </div>
+                      <div className="basura">
+                        <a onClick={() => this.eliminar(product.codetalle)}>
+                          <BsFillTrash3Fill />
+                        </a>
+                      </div>
+                    </div>
+                  );
+                })}
+            </div>
+          )}
           </ModalBody>
 
 
@@ -166,6 +167,10 @@ class Carrito extends Component {
             <div className="primero" id="modalF">
             <div className="subtotal" id="modalF"> Subtotal:</div>
             <div className="preciosub" id="modalF">{this.state.subtotal} Bs.</div>
+            </div>
+            <div className="segundo" id="modalF">
+             <div className="descuento" id="modalF"> Despacho:</div>
+             <div className="despacho" id="modalF">{this.state.despacho} producto(s)</div>
             </div>
             <div className="segundo" id="modalF">
              <div className="descuento" id="modalF"> Descuento:</div>
